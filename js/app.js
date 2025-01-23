5656047fc57ecadfc4948e370e716a4ee9841e90
Susu-spec <suleimansuwaibat@gmail.com>
2025-01-23 18:16:49 +0100

    feat: add filter by selected value options

--- js/app.js
+++ js/app.js
@@ -2,9 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const select_group = document.getElementById("genre-filter");
     const movie_cards = document.getElementById("movie-grid");
 
+    let allMovies = [];
     function fetchMovieData() {
-    
-        
         fetch("./js/data/movies.json")
             .then((res) => {
                 if (!res.ok) {
@@ -15,7 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
             })
             .then((data) => {
                 const genres = data.genres;
-                const movies = data.movies;
+                allMovies = data.movies;
 
                 genres.forEach((genre) => {
                     const option = document.createElement("option");
@@ -25,18 +24,52 @@ document.addEventListener("DOMContentLoaded", () => {
                     select_group.append(option)
                 })
 
-                movies.forEach((movie) => {
-                    const movie_card = document.createElement("div");
-                    movie_card.textContent = movie.title;
-
-                    movie_cards.append(movie_card);
-                })
-
+                renderMovies(allMovies)
             })
             .catch((error) => 
                 console.error("Unable to fetch data", error));
     }
 
-    fetchMovieData();
 
-})
+
+
+   
+
+    
+    function renderMovies(movies) {
+        // Clear the movie grid each time we render a new set (filteredData or not)
+        movie_cards.innerHTML = "";
+
+        movies.forEach((movie) => {
+            const movie_card = document.createElement("div");
+            movie_card.classList.add("movie-card");
+            movie_card.innerHTML = `
+            <img src="${movie.posterUrl}" alt="${movie.title} Poster" class="movie-poster">
+            <h3 class="movie-title">${movie.title}</h3>
+            <p class="movie-meta">${movie.genres.join(", ")}</p>
+            <p class="movie-year">${movie.year}</p>
+        `;
+            movie_cards.append(movie_card);
+        });
+    }
+
+    select_group.addEventListener("change", () => {
+        const selectedGenre = select_group.value;
+        console.log(selectedGenre)
+        // Filter movies based on the selected genre
+        const filteredMovies = selectedGenre === "all"
+            ? allMovies
+            : allMovies.filter(movie => movie.genres.some(genre => genre.toLowerCase() === selectedGenre));
+
+
+            renderMovies(filteredMovies);
+        
+
+    });
+
+        fetchMovieData();
+});
+
+
+
+
