3857fbbcf98ea1ec98b1b55be6dea5904b97e10e
Susu-spec <suleimansuwaibat@gmail.com>
2025-01-23 15:36:28 +0100

    feat: add select option content

--- js/app.js
+++ js/app.js
@@ -1,18 +1,32 @@
-function fetchMovieData() {
-	fetch("./js/data/movies.json")
-	      .then((res) => {
-		      if (!res.ok) {
-			      throw new Error
-			           (`HTTP error! Status: ${res.status}`);
-		      }
-		      return res.json();
-	      })
-	      .then((data) => {
-		      const genres = data.genres;
-		      console.log(genres);
-	      })
-	      .catch((error) => 
-		      console.error("Unable to fetch data", error));
-}
+document.addEventListener("DOMContentLoaded", () => {
+    function fetchMovieData() {
+    
+        const select_group = document.getElementById("genre-filter");
+        fetch("./js/data/movies.json")
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error
+                        (`HTTP error! Status: ${res.status}`);
+                }
+                return res.json();
+            })
+            .then((data) => {
+                const genres = data.genres;
+                const movies = data.movies;
 
-fetchMovieData();
+                genres.forEach((genre) => {
+                    const option = document.createElement("option");
+                    option.value = genre.toLowerCase();
+                    option.textContent = genre;
+                    // append each created option with genre
+                    select_group.append(option)
+                })
+
+            })
+            .catch((error) => 
+                console.error("Unable to fetch data", error));
+    }
+
+    fetchMovieData();
+
+})
